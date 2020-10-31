82a090823eafe29b00f7fab10d2be7e8fccdcac8
ffzs <dz759043338@163.com>
2020-10-31 18:36:44 +0800

    update

---
+++ src/main/java/com/ffzs/quark/reactive_demo/HelloWorldRoutes.java
@@ -0,0 +1,23 @@
+package com.ffzs.quark.reactive_demo;
+
+import io.quarkus.vertx.web.Route;
+import io.quarkus.vertx.web.RouteBase;
+import io.smallrye.mutiny.Uni;
+import io.vertx.core.http.HttpMethod;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+/**
+ * @Author: ffzs
+ * @Date: 2020/10/31 下午6:32
+ */
+
+@RouteBase(path = "/", produces = "application/json")
+public class HelloWorldRoutes {
+
+    @Route(methods = HttpMethod.GET, path = "/")
+    public Uni<String> getAll() throws UnknownHostException {
+        return Uni.createFrom().item("Hello world" + InetAddress.getLocalHost().toString());
+    }
+
+}
